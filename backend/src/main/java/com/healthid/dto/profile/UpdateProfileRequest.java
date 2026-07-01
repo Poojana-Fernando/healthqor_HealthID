77c55812ddc5235c3ba005b9f04812f50a9a1124
Dineshakodikara <dineshakodikara89@gmail.com>
2026-07-01 21:08:26 +0530

    updateprofilerequest

--- backend/src/main/java/com/healthid/dto/profile/UpdateProfileRequest.java
+++ backend/src/main/java/com/healthid/dto/profile/UpdateProfileRequest.java
@@ -1,6 +1,8 @@
 package com.healthid.dto.profile;
 
 import com.healthid.entity.Gender;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 import java.math.BigDecimal;
@@ -10,8 +12,13 @@ import java.util.List;
 @Data
 public class UpdateProfileRequest {
 
+    @NotBlank(message = "Name cannot be blank")
     private String name;
+
+    @NotBlank(message = "Mobile cannot be blank")
+    @Pattern(regexp = "^\\+[1-9]\\d{7,14}$", message = "Mobile must be E.164 format")
     private String mobile;
+
     private Gender gender;
     private String bloodType;
     private BigDecimal heightCm;
